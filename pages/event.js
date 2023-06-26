c7def4e838ff3bf22599b1aa75ccd54e17332161
sujith-rek <ps335@snu.edu.in>
2023-06-26 20:01:55 +0530

    back to normal ;))

--- pages/event.js
+++ pages/event.js
@@ -1,13 +1,12 @@
 import HeroSectionEvent from "@/components/HeroSection2/HeroSectionEvent";
 import '../components/HeroSection2/HeroSectionEvent.scss'
 import ClubEventCard from "@/components/ClubEventCards/ClubEventCard";
-import ClubEvents from "@/components/ClubEvents/ClubEvents";
 function Event()
 {
     return(
         <section className="HeroSectionContainer-event">
             <HeroSectionEvent/>
-            <ClubEvents/>
+            <ClubEventCard/>
         </section>
     )
 }
