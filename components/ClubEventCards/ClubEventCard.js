1da81809e3b358dbf90ad6c75fbe871171e4bc06
sujith-rek <ps335@snu.edu.in>
2023-06-26 18:30:11 +0530

    code cleanup

--- components/ClubEventCards/ClubEventCard.js
+++ components/ClubEventCards/ClubEventCard.js
@@ -22,126 +22,87 @@ const ClubEventCard = () => {
             "date": "Tuesday, 4th April",
             "time": "10am Onwards",
             "venue":"G Block Auditorium"
-        }
+        },{
+            "Hero_image": '/Images/event_cards/hero_img.png',
+            "Logo_img": '/Images/event_cards/logo_img.png',
+            "Title": "Ragnarok",
+            "Club_name": "Entrepreneur Cell",
+            "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien fringilla, mattis ligula consectetur, ultrices mauris. Maecenas vitae mattis tellus.",
+            "date": "Tuesday, 4th April",
+            "time": "10am Onwards",
+            "venue":"G Block Auditorium"
+        },
+        {
+            "Hero_image": '/Images/event_cards/hero_img.png',
+            "Logo_img": '/Images/event_cards/logo_img.png',
+            "Title": "Ragnarok",
+            "Club_name": "Entrepreneur Cell",
+            "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien fringilla, mattis ligula consectetur, ultrices mauris. Maecenas vitae mattis tellus.",
+            "date": "Tuesday, 4th April",
+            "time": "10am Onwards",
+            "venue":"G Block Auditorium"
+        },{
+            "Hero_image": '/Images/event_cards/hero_img.png',
+            "Logo_img": '/Images/event_cards/logo_img.png',
+            "Title": "Ragnarok",
+            "Club_name": "Entrepreneur Cell",
+            "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien fringilla, mattis ligula consectetur, ultrices mauris. Maecenas vitae mattis tellus.",
+            "date": "Tuesday, 4th April",
+            "time": "10am Onwards",
+            "venue":"G Block Auditorium"
+        },
+        {
+            "Hero_image": '/Images/event_cards/hero_img.png',
+            "Logo_img": '/Images/event_cards/logo_img.png',
+            "Title": "Ragnarok",
+            "Club_name": "Entrepreneur Cell",
+            "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien fringilla, mattis ligula consectetur, ultrices mauris. Maecenas vitae mattis tellus.",
+            "date": "Tuesday, 4th April",
+            "time": "10am Onwards",
+            "venue":"G Block Auditorium"
+        },
         ]
     return (
-        EventDetails.map((item) => {
-            return (
-        <div key={item.name} className="EventCard-tray">
-            <div className="EventCard">
-                <div className="EventCard-club_img">
-                    <img src={item.Hero_image} className="EventCard-club_details__img1"/>
-                </div>
-                <div className="EventCard-club_details">
-                    <img src={item.Logo_img} className="EventCard-club_details__img2"/>
-                    <div className="EventCard-club_event-details">
-                        <div className="EventCard-club_event">
-                            <p className="EventCard-club_event-title">{item.Title}</p>
+        <div className="ClubEventCard">
+            <div className="ClubEventCard__container">
+                {EventDetails.map((Cevent) => {
+                    return(
+                    <div className="ClubEventCard__card">
+                        <div className="ClubEventCard__card__heroimg">
+                            <img src={Cevent.Hero_image} alt="Hero Image"/>
                         </div>
-                        <div className="EventCard-club_name">
-                            <p className="EventCard-club_event-club">{item.Club_name}</p>
+                        <div className='ClubEventCard__card__club'>
+                            <img src={Cevent.Logo_img} className='ClubEventCard__card__club--logo'/>
+                            <div className="ClubEventCard__Card__club__details">
+                                <p className='ClubEventCard__club__details--title'>{Cevent.Title}</p>
+                                <p className='ClubEventCard__club__details--club'>{Cevent.Club_name}</p>
+                            </div>
                         </div>
-                    </div>
-                </div>
-                <div className="EventCard-event_content">
-                    <p>{item.content}</p>
-                </div>
-                <div className="EventCard-event_details">
-                    <div className="EventCard-event_details-date">
-                        <p className="EventCard-event_details-all">Date</p>
-                        <p className="EventCard-event_details-out">{item.date}</p>
-                    </div>
-                    <div className="EventCard-event_details-time">
-                        <p className="EventCard-event_details-all">Time</p>
-                        <p className="EventCard-event_details-out">{item.time}</p>
-                    </div>
-                    <div className="EventCard-event_details-venue">
-                        <p className="EventCard-event_details-all">Venue</p>
-                        <p className="EventCard-event_details-out">{item.venue}</p>
-                    </div>
-                </div>
-                <div className="EventCard-enterin">
-                    <p>Event Report</p>
-                    <span><IoIosArrowRoundForward/></span>
-                </div>
-            </div>
-            <div className="EventCard">
-                <div className="EventCard-club_img">
-                    <img src="/Images/event_cards/hero_img.png" className="EventCard-club_details__img1"/>
-                </div>
-                <div className="EventCard-club_details">
-                    <img src="/Images/event_cards/logo_img.png" className="EventCard-club_details__img2"/>
-                    <div className="EventCard-club_event-details">
-                        <div className="EventCard-club_event">
-                            <p className="EventCard-club_event-title">Ragnarok</p>
+                        <div className='ClubEventCard__card__description'>
+                            <p className='ClubEventCard__card__description--content'>{Cevent.content}</p>
                         </div>
-                        <div className="EventCard-club_name">
-                            <p className="EventCard-club_event-club">Entrepreneur Cell</p>
+                        <div className='ClubEventCard__card__details'>
+                            <div className='ClubEventCard__card__details--container'>
+                                <p className='ClubEventCard__card__details--container__title'>Date</p>
+                                <p className='ClubEventCard__card__details--container__value'>{Cevent.date}</p>
+                            </div>
+                            <div className='ClubEventCard__card__details--container'>
+                                <p className='ClubEventCard__card__details--container__title'>Time</p>
+                                <p className='ClubEventCard__card__details--container__value'>{Cevent.time}</p>
+                            </div>
+                            <div className='ClubEventCard__card__details--container'>
+                                <p className='ClubEventCard__card__details--container__title'>Venue</p>
+                                <p className='ClubEventCard__card__details--container__value'>{Cevent.venue}</p>
+                            </div>
+                        </div>
+                        <div className='ClubEventCard__Card__report'>
+                            <button className='ClubEvent__Card__report--button'>
+                            Event Report<span><IoIosArrowRoundForward/></span></button>
                         </div>
                     </div>
-                </div>
-                <div className="EventCard-event_content">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien fringilla, mattis ligula consectetur, ultrices mauris. Maecenas vitae mattis tellus.</p>
-                </div>
-                <div className="EventCard-event_details">
-                    <div className="EventCard-event_details-date">
-                        <p className="EventCard-event_details-all">Date</p>
-                        <p className="EventCard-event_details-out">Tuesday, 4th April</p>
-                    </div>
-                    <div className="EventCard-event_details-time">
-                        <p className="EventCard-event_details-all">Time</p>
-                        <p className="EventCard-event_details-out">10am Onwards</p>
-                    </div>
-                    <div className="EventCard-event_details-venue">
-                        <p className="EventCard-event_details-all">Venue</p>
-                        <p className="EventCard-event_details-out">G Block Auditorium</p>
-                    </div>
-                </div>
-                <div className="EventCard-enterin">
-                    <p>Event Report</p>
-                    <span><IoIosArrowRoundForward/></span>
-                </div>
-            </div>
-            <div className="EventCard">
-                <div className="EventCard-club_img">
-                    <img src="/Images/event_cards/hero_img.png" className="EventCard-club_details__img1"/>
-                </div>
-                <div className="EventCard-club_details">
-                    <img src="/Images/event_cards/logo_img.png" className="EventCard-club_details__img2"/>
-                    <div className="EventCard-club_event-details">
-                        <div className="EventCard-club_event">
-                            <p className="EventCard-club_event-title">Ragnarok</p>
-                        </div>
-                        <div className="EventCard-club_name">
-                            <p className="EventCard-club_event-club">Entrepreneur Cell</p>
-                        </div>
-                    </div>
-                </div>
-                <div className="EventCard-event_content">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien fringilla, mattis ligula consectetur, ultrices mauris. Maecenas vitae mattis tellus.</p>
-                </div>
-                <div className="EventCard-event_details">
-                    <div className="EventCard-event_details-date">
-                        <p className="EventCard-event_details-all">Date</p>
-                        <p className="EventCard-event_details-out">Tuesday, 4th April</p>
-                    </div>
-                    <div className="EventCard-event_details-time">
-                        <p className="EventCard-event_details-all">Time</p>
-                        <p className="EventCard-event_details-out">10am Onwards</p>
-                    </div>
-                    <div className="EventCard-event_details-venue">
-                        <p className="EventCard-event_details-all">Venue</p>
-                        <p className="EventCard-event_details-out">G Block Auditorium</p>
-                    </div>
-                </div>
-                <div className="EventCard-enterin">
-                    <p>Event Report</p>
-                    <span><IoIosArrowRoundForward/></span>
-                </div>
+                );})}
             </div>
         </div>
-            );
-        })
     );
 };
 
