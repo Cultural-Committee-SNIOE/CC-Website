6e1a24d617711530c74c51795186028d7d4185e6
Chaitanya Tandon <ct765@snu.edu.in>
2023-06-26 19:29:20 +0530

    feat: ClubPage Completed

--- pages/clubPage.js
+++ pages/clubPage.js
@@ -1,10 +1,13 @@
 const { default: ClubPageHeader } = require("@/components/ClubPageHeader/ClubPageHeader");
+const { default: ClubPageCardSection } = require("@/components/ClubPageCardSection/ClubPageCardSection");
+
 import '../styles/root/clubPage.scss'
 function ClubPage()
 {
     return(
         <section className="ClubPageHeader">
             <ClubPageHeader />
+            <ClubPageCardSection />
         </section>
     )
 }
